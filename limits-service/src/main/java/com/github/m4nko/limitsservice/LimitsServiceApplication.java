e695674d5028142496b1f9fd16e284aad4833c07
Carlos Pereira <carlospereira@ciandt.com>
2023-05-05 09:37:46 -0300

    Update: generated new intelij project due to language version problem, updated to java 17

--- limits-service/src/main/java/com/github/m4nko/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/github/m4nko/limitsservice/LimitsServiceApplication.java
@@ -1,4 +1,4 @@
-package com.m4nko.microservices.limitsservice;
+package com.github.m4nko.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- limits-service/src/test/java/com/github/m4nko/limitsservice/LimitsServiceApplicationTests.java
+++ limits-service/src/test/java/com/github/m4nko/limitsservice/LimitsServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.m4nko.microservices.limitsservice;
+package com.github.m4nko.limitsservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
