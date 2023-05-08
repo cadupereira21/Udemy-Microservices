06e752ab0e94b1fc1be0193e12e2051afc06d528
Carlos Pereira <carlospereira@ciandt.com>
2023-05-08 09:25:39 -0300

    Create: implemented Configuration class to get limits value from application.properties file

--- limits-service/src/main/java/com/github/m4nko/limitsservice/control/LimitsController.java
+++ limits-service/src/main/java/com/github/m4nko/limitsservice/control/LimitsController.java
@@ -1,14 +1,21 @@
 package com.github.m4nko.limitsservice.control;
 
 import com.github.m4nko.limitsservice.bean.Limits;
+import com.github.m4nko.limitsservice.config.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration config;
+
     @GetMapping("/limits")
     public Limits retrieveLimits(){
-        return new Limits(1, 1000);
+
+        return new Limits(config.getMinimum(), config.getMaximum());
+        //return new Limits(1, 1000);
     }
 }
