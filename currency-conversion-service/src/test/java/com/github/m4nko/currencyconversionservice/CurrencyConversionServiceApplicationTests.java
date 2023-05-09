23c66ce482599381adeac96e0d70d127e23b1661
Carlos Pereira <carlospereira@ciandt.com>
2023-05-09 11:23:56 -0300

    Create: added currency services as submodules, created currency-conversion submodule

---
+++ currency-conversion-service/src/test/java/com/github/m4nko/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.m4nko.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
