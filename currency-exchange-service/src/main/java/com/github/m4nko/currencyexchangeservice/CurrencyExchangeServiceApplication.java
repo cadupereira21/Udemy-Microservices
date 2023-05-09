f9164b7ede29fdfec021150c93ca08921118458b
Carlos Pereira <carlospereira@ciandt.com>
2023-05-09 09:26:50 -0300

    Create: implemented currency-exchange microservice project

---
+++ currency-exchange-service/src/main/java/com/github/m4nko/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.m4nko.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
---
+++ currency-exchange-service/src/test/java/com/github/m4nko/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.m4nko.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
