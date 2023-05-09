a6d49c5853091b242faf7f7cae8b23f43cefc63c
Carlos Pereira <carloseduardo2101@gmail.com>
2023-05-09 11:46:44 -0300

    Create: implemented CurrencyConversion and CurrencyConversionController

--- currency-conversion-service/src/main/java/com/github/m4nko/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/github/m4nko/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,5 +1,19 @@
 package com.github.m4nko.currencyconversionservice.controller;
 
+import com.github.m4nko.currencyconversionservice.entity.CurrencyConversion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
 public class CurrencyConversionController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateConversion(@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity){
+
+        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+    }
 
 }
