cc54fd16f088d64b3dee25015cbac5413663acd4
Carlos Pereira <carloseduardo2101@gmail.com>
2023-05-09 14:18:37 -0300

    Update: implemented calculateConversion method to get values from currency-exchange

--- currency-conversion-service/src/main/java/com/github/m4nko/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/github/m4nko/currencyconversionservice/controller/CurrencyConversionController.java
@@ -4,8 +4,10 @@ import com.github.m4nko.currencyconversionservice.entity.CurrencyConversion;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
@@ -13,7 +15,27 @@ public class CurrencyConversionController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateConversion(@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity){
 
-        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+
+        var responseEntity = new RestTemplate().getForEntity(
+                "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversion.class,
+                uriVariables
+        );
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()
+        );
     }
 
 }
