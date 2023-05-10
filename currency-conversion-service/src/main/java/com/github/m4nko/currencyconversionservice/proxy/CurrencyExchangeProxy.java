482e6fca1dfa6c88e33d0f1cd2d6b6d42bb82fe1
Carlos Pereira <carloseduardo2101@gmail.com>
2023-05-10 09:01:57 -0300

    Update: changed proxy to automatic load balance between instances of currency-exchange

--- currency-conversion-service/src/main/java/com/github/m4nko/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/github/m4nko/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange", url = "localhost:8000") // Use the name of the microservice u want to use
+//@FeignClient(name="currency-exchange", url = "localhost:8000") // Use the name of the microservice u want to use
+@FeignClient(name="currency-exchange") // Removing the url lets load balance happen
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
