ae3542286ddcdf9d7a005d45e5470a10bd33e955
Carlos Pereira <carloseduardo2101@gmail.com>
2023-05-10 13:46:23 -0300

    Create: implemented retry functionality for when request throws exception

--- currency-exchange-service/src/main/java/com/github/m4nko/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/github/m4nko/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,13 +1,23 @@
 package com.github.m4nko.currencyexchangeservice.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("/sample-api")
+    @Retry(name = "sample-api")
     public String sampleApi(){
-        return "Sample API";
+        logger.info("Sample API call received");
+        var entity = new RestTemplate().getForEntity("http://localhost:8080/dummy-api", String.class);
+
+        return entity.getBody();
     }
 }
